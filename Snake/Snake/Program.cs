2044299a66a66bb57e1f17f257ccc826dc95013e
enenken <enenken@mail.ru>
2015-10-04 19:29:55 +0300

    потренировался со списками

--- Snake/Snake/Program.cs
+++ Snake/Snake/Program.cs
@@ -12,13 +12,36 @@ namespace Snake
         {
             Point p1 = new Point(1,3,'*');
           
-            p1.Draw();
+            //p1.Draw();
 
             Point p2 = new Point(4,5,'*');
-           
-            p2.Draw();
 
+            //p2.Draw();
 
+            Point p3 = new Point(10, 10, '%');
+            Point p4 = new Point(9, 1, '&');
+
+            List<int> spisok = new List<int>();
+            spisok.Add(9);
+            spisok.Add(1);
+            spisok.Add(1);
+
+            foreach(int i in spisok)
+            {
+                Console.WriteLine(i);
+
+            }
+
+            List<Point> tochki = new List<Point>();
+            tochki.Add(p1);
+            tochki.Add(p2);
+            tochki.Add(p3);
+            tochki.Add(p4);
+            foreach (Point i in tochki)
+            {
+                i.Draw();
+
+            }
 
             Console.ReadLine();
         }
